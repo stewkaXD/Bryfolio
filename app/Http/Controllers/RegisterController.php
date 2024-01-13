16089bb3b49468a539766a03bdd9ec8fda21957a
Bryan Amds <bryan.amadeus1003@gmail.com>
2024-01-13 17:57:12 +0700

    Sesi 15 - User Registration

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -1,8 +1,10 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\User;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
 {
@@ -12,5 +14,24 @@ class RegisterController extends Controller
             'title' => 'Register',
             'active' => 'register'
         ]);
+    }
+
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required|max:255',
+            'username' => 'required|min:3|max:255|unique:users',
+            'email' => 'required|email:dns|unique:users',
+            'password' => 'required|min:8|max:255'
+        ]);
+
+        // $validatedData['password'] = bcrypt($validatedData['password']);
+        $validatedData['password'] = Hash::make($validatedData['password']);
+
+        User::create($validatedData);
+
+        session()->flash('success', 'Succesfully registered!');
+
+        return redirect('/login');
     }
 }
--- resources/views/register/index.blade.php
+++ resources/views/register/index.blade.php
@@ -7,26 +7,47 @@
 
         <main class="form-register">
             <h1 class="h3 mb-3 fw-normal text-center">REGISTRATION</h1>
-            <form>
+            <form action="/register" method="POST">
 
+                @csrf
                 <div class="form-floating">
-                    <input type="text" name="name" class="form-control rounded-top" id="floatingName" placeholder="Name">
+                    <input type="text" name="name" class="form-control rounded-top @error('name') is-invalid @enderror" id="floatingName" placeholder="Name" value="{{ old('name') }}" required>
                     <label for="floatingName">Name</label>
+                    @error('name')
+                        <div class="invalid-feedback">
+                            {{ $message }}
+                        </div>   
+                    @enderror   
                 </div>
 
                 <div class="form-floating">
-                    <input type="text" name="username" class="form-control" id="floatingUsername" placeholder="Username">
+                    <input type="text" name="username" class="form-control @error('username') is-invalid @enderror" id="floatingUsername" placeholder="Username" value="{{ old('username') }}"  required>
                     <label for="floatingUsername">Username</label>
+                    @error('username')
+                        <div class="invalid-feedback">
+                            {{ $message }}
+                        </div>   
+                    @enderror
                 </div>
         
                 <div class="form-floating">
-                    <input type="email" name="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+                    <input type="email" name="email" class="form-control @error('email') is-invalid @enderror" id="floatingInput" placeholder="name@example.com" value="{{ old('email') }}"  required>
                     <label for="floatingInput">Email address</label>
+                    @error('email')
+                        <div class="invalid-feedback">
+                            {{ $message }}
+                        </div>   
+                    @enderror
                 </div>
         
                 <div class="form-floating">
-                    <input type="password" name="password" class="form-control rounded-bottom" id="floatingPassword" placeholder="Password">
+                    <input type="password" name="password" class="form-control rounded-bottom @error('password') is-invalid @enderror" id="floatingPassword" placeholder="Password" required>
                     <label for="floatingPassword">Password</label>
+                    @error('password')
+                        <div class="invalid-feedback">
+                            {{ $message }}
+                        </div>   
+                    @enderror
                 </div>
         
                 <button class="btn btn-primary w-100 py-2 mt-3" type="submit">Register!</button>
